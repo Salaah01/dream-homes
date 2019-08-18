c1e23193a8dab8cdb147e054fa221c2563b6e244
Salaah <salaah_amin@hotmail.co.uk>
2019-08-18 14:38:10 +0100

    added admin functions for wedding messages

--- wedding_messages/admin.py
+++ wedding_messages/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import WeddingMessages
 
-# Register your models here.
+class WeddingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'fromNames', 'message')
+    list_display_links = ('id', 'fromNames')
+    list_filter = ('fromNames',)
+    search_fields = ('fromNames',)
+
+admin.site.register(WeddingMessages, WeddingAdmin)
